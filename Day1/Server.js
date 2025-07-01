310b8751aeda9a0f47316dfaa1f0816723a6d0fd
Cyber-Rebel <np103177@gmail.com>
2025-07-01 20:23:29 +0530

    Day 1

---
+++ Day1/Server.js
@@ -0,0 +1,18 @@
+const http = require('http') // file ko  reauire ab file ke method use kar sakte hae .createSErver etc..
+
+// function temp (){
+//     return a+b;
+// }
+// es code ko run karne ke liye aapko temp() call karna padega ese ke http ko use karn cerateServer ko call karna padga 
+// server crate ho gaya hae 
+const server=http.createServer((req,res)=>{
+    res.end('hellp world server')
+}) 
+// server ko program kar diya hae 
+server.listen(3000,()=>{
+    console.log("server is runnig on port 3000")
+})
+
+
+
+// server.js ke ffile ko run karne ke liye tume node_modules jarut nahi but index.js cat package ke vajt se lagela
