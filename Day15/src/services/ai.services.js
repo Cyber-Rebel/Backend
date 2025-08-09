d0df51de14dbf9ad7fb5e77774bd06ef84ba27ef
Cyber-Rebel <np103177@gmail.com>
2025-08-10 02:14:40 +0530

    Day 15 remain

---
+++ Day15/src/services/ai.services.js
@@ -0,0 +1,17 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({
+    apiKey:''
+});
+
+async function generateResponce(Chathistory) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: Chathistory // Use the Chathistory array directly
+ } );
+//   console.log(response.text);
+  return response.text;
+}
+
+module.exports = {generateResponce}
+// only for text to text message convertion 
